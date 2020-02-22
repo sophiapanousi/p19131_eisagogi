619d5bb64e4b0d6578473d4b412dea2feca1102a
sophiapanousi <sophiapanousi@gmail.com>
2020-02-22 23:55:58 +0200

    22/2

--- Ask3.py
+++ Ask3.py
@@ -1,30 +1,32 @@
 # Γράψτε μια συνάρτηση σε Python η οποία υπολογίζει το ποσό πληρωμής
 # όταν της περνάει ο χρήστης τις εγγραφές ενός λεξικού σε λίστα
 # για το τι αγόρασε και το ποσοστό φόρου.
-# Άσκηση 3,8,9,12
-# list of numbers 
+# Άσκηση 3
+
+# Έχουμε δημιουργήσει ένα λεξικό με εικονικά προϊόντα και τιμές για να δοκιμάσουμε την 
+# συνάρτηση
 import csv
 
- # Function to print sum 
+# Συνάρτηση για τον υπολογσιμό του συνολικού φόρου.
+# Δέχεται σαν παραμέτρους ένα λεξικό και το ποσοστό του φόρου
 def returnSum(myDict, foros): 
     sum = 0
     for i in myDict: 
-        #print (myDict[i])
+        #Χρησιμοποιούμε τη μέθοδο float για ομογενοποίηση των τιμών
         sum = sum + float(myDict[i])
     return sum *((100+foros)/100)
 
-# Driver Function 
-duck = {'a': 100, 'b':200, 'c':300,'eliza':0.00025} 
-#Eliza is my sister
 
+sample_dict = {'a': 100, 'b':200, 'c':300, 'd':100 } 
+
+#εισαγωγή στοιχείων απο το συνοδευτικό csv αρχείο και πέρασμα σε λεξικό 
 reader = csv.reader(open('mock_data_dict.csv', 'r'))
 mydict = {}
 for row in reader:
    k, v = row
    mydict[k] = v
 
-#for i in mydict:
-#   print (i, mydict[i])
+#Δοκιμή της συνάρτησης με τα 2 σετ δεδομένων
+print("To συνολικό ποσό που πρέπει να πληρωθεί είναι :", returnSum(mydict,24)) 
+print("To συνολικό ποσό που πρέπει να πληρωθεί είναι :", returnSum(sample_dict,19)) 
 
-
-print("Sum :", returnSum(duck,24)) 
